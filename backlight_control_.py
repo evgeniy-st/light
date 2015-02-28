a49c13dc363b90b602a9fbc7c1cb714b9dde88b3
evgeniy-st <stoyushko.evgeniy@gmail.com>
2015-02-28 12:22:04 +0300

    Insert timer

--- backlight_control_.py
+++ backlight_control_.py
@@ -3,19 +3,14 @@ import subprocess
 import dbus
 import os
 from time import sleep
+from threading import Timer
 
 IMAGE_PATH = '/home/stea/temp/cam.jpg'
 BRIGHTNESS = [50, 50]
-
-
-def getBrightness():
-    if len(BRIGHTNESS) >= 3:
-        BRIGHTNESS.pop(0)
-    create_image_fswebcam(IMAGE_PATH)
-    BRIGHTNESS.append(calc_avgcolor(IMAGE_PATH))
-    del_image(IMAGE_PATH)
-    current_brightness = int(round(sum(BRIGHTNESS)/len(BRIGHTNESS), 0))
-    return current_brightness
+UPDATE_BRIGHTNESS_TIMEOUT = 3
+UPDATE_DISPLAY_TIMEOUT = 5
+UPDATE_KBD_TIMEOUT = 0.5
+IDLE_KBD_TIMEOUT = 60000
 
 
 def create_image_fswebcam(path):
@@ -32,26 +27,39 @@ def calc_avgcolor(path):
     return value
 
 
-def setDisplayBacklight_xbacklight(value):
-    command = '/usr/bin/xbacklight -set ' + str(value)
-    subprocess.call(command, shell=True)
+def del_image(path):
+    os.remove(path)
 
 
-def setDisplayBacklight_dbus(value):
-    bus = dbus.SessionBus()
-    obj = bus.get_object('org.kde.Solid.PowerManagement',
-                         '/org/kde/Solid/PowerManagement/Actions/BrightnessControl')
-    interface = dbus.Interface(obj, dbus_interface='org.kde.Solid.PowerManagement.Actions.BrightnessControl')
-    interface.setBrightness(dbus.Int32(value))
+def updateBrightness():
+    if len(BRIGHTNESS) >= 5:
+        BRIGHTNESS.pop(0)
+    create_image_fswebcam(IMAGE_PATH)
+    BRIGHTNESS.append(calc_avgcolor(IMAGE_PATH))
+    del_image(IMAGE_PATH)
+    Timer(UPDATE_BRIGHTNESS_TIMEOUT, updateBrightness).start()
 
 
-def getDisplayBacklight_dbus():
-    bus = dbus.SessionBus()
-    obj = bus.get_object('org.kde.Solid.PowerManagement',
-                         '/org/kde/Solid/PowerManagement/Actions/BrightnessControl')
-    interface = dbus.Interface(obj, dbus_interface='org.kde.Solid.PowerManagement.Actions.BrightnessControl')
-    result = int(interface.brightness())
-    return result
+def getBrightnessValue():
+    current_brightness = int(round(sum(BRIGHTNESS)/len(BRIGHTNESS), 0))
+    return current_brightness
+
+
+def setKbdBacklight(value):
+    brightness = int(round(value/12, 0))
+    idle = getPowerSaveStatus_dbus()
+    idle_time = 60000
+    if idle < idle_time:
+        if brightness <= 3:
+            if brightness == 0:
+                brightness = 1
+            if getKbdBacklight_dbus() != brightness:
+                setKbdBacklight_dbus(brightness)
+        else:
+            if getKbdBacklight_dbus() != brightness:
+                setKbdBacklight_dbus(0)
+    else:
+        setKbdBacklight_dbus(0)
 
 
 def setKbdBacklight_dbus(value):
@@ -83,21 +91,11 @@ def getPowerSaveStatus_dbus():
     result = int(interface.GetSessionIdleTime())
     return result
 
-def del_image(path):
-    os.remove(path)
 
-def setDisplayBacklight(value):
-    if value <= 90:
-        value += 10
-    brightness = round(value, -1)
-    if getDisplayBacklight_dbus != brightness:
-        setDisplayBacklight_dbus(brightness)
-
-def setKbdBacklight(value):
-    brightness = int(round(value/12, 0))
+def updateKbd():
     idle = getPowerSaveStatus_dbus()
-    idle_time = 60000
-    if idle < idle_time:
+    if idle < IDLE_KBD_TIMEOUT:
+        brightness = int(round(getBrightnessValue()/12, 0))
         if brightness <= 3:
             if brightness == 0:
                 brightness = 1
@@ -108,24 +106,39 @@ def setKbdBacklight(value):
                 setKbdBacklight_dbus(0)
     else:
         setKbdBacklight_dbus(0)
+    Timer(UPDATE_KBD_TIMEOUT, updateKbd).start()
+
+
+def setDisplayBacklight_dbus(value):
+    bus = dbus.SessionBus()
+    obj = bus.get_object('org.kde.Solid.PowerManagement',
+                         '/org/kde/Solid/PowerManagement/Actions/BrightnessControl')
+    interface = dbus.Interface(obj, dbus_interface='org.kde.Solid.PowerManagement.Actions.BrightnessControl')
+    interface.setBrightness(dbus.Int32(value))
+
+
+def getDisplayBacklight_dbus():
+    bus = dbus.SessionBus()
+    obj = bus.get_object('org.kde.Solid.PowerManagement',
+                         '/org/kde/Solid/PowerManagement/Actions/BrightnessControl')
+    interface = dbus.Interface(obj, dbus_interface='org.kde.Solid.PowerManagement.Actions.BrightnessControl')
+    result = int(interface.brightness())
+    return result
+
+
+def setDisplayBacklight(value):
+    if value <= 90:
+        value += 10
+    brightness = round(value, -1)
+    if getDisplayBacklight_dbus() != brightness:
+        setDisplayBacklight_dbus(brightness)
+
+
+def updateDisplay():
+    setDisplayBacklight(getBrightnessValue())
+    Timer(UPDATE_DISPLAY_TIMEOUT, updateDisplay).start()
 
 if __name__ == '__main__':
-    l = []
-    previous_brightness = 50
-    current_brightness = 50
-    idle = 0
-    while True:
-        create_image_fswebcam(IMAGE_PATH)
-        l.append(calc_avgcolor(IMAGE_PATH))
-        del_image(IMAGE_PATH)
-        if len(l) == 3:
-            l.pop(0)
-        current_brightness = int(round(sum(l)/len(l), 0))
-        idle = getPowerSaveStatus_dbus()
-        print(idle)
-        print(current_brightness)
-        if abs(previous_brightness - current_brightness) > 3:
-            previous_brightness = current_brightness
-            setDisplayBacklight(current_brightness)
-        setKbdBacklight(current_brightness)
-        sleep(5)
+    updateBrightness()
+    updateKbd()
+    updateDisplay()
