a7baf3619fbf0566a9ccbe8dcda342c00b58ea6f
evgeniy-st <stoyushko.evgeniy@gmail.com>
2015-02-28 10:17:29 +0300

    add getBrighness()

--- backlight_control_.py
+++ backlight_control_.py
@@ -5,6 +5,18 @@ import os
 from time import sleep
 
 IMAGE_PATH = '/home/stea/temp/cam.jpg'
+BRIGHTNESS = [50, 50, 50]
+
+
+def getBrightness():
+    global BRIGHTNESS
+    create_image_fswebcam(IMAGE_PATH)
+    BRIGHTNESS.append(calc_avgcolor(IMAGE_PATH))
+    del_image(IMAGE_PATH)
+    if len(BRIGHTNESS) == 3:
+        BRIGHTNESS.pop(0)
+    current_brightness = int(round(sum(l)/len(l), 0))
+    return current_brightness
 
 
 def create_image_fswebcam(path):
